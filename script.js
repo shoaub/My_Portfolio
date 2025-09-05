88d633aae0f7dc960dd912d77762508952fb4f20
Shoaib Alam <135745217+shoaub@users.noreply.github.com>
2025-09-05 13:40:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.getElementById("theme-toggle");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  if (document.body.classList.contains("dark")) {
+    toggleBtn.textContent = "☀️ Light Mode";
+  } else {
+    toggleBtn.textContent = "🌙 Dark Mode";
+  }
+});
